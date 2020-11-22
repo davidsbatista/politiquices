2fda0dd4e1ee77ccf7c3786f8a5175d796bcedfd
David Soares Batista <david.batista@comtravo.com>
2020-11-22 23:51:37 +0100

    WIP: search box

---
+++ politiquices/webapp/webapp/populate_search_box.py
@@ -0,0 +1,41 @@
+import json
+
+from politiquices.webapp.webapp.app.sparql_queries import query_sparql, prefixes
+
+
+def get_persons():
+    query = """
+        SELECT DISTINCT ?personLabel ?item ?image_url {
+        ?item wdt:P31 wd:Q5 # get all human entities from the local graph
+        SERVICE <https://query.wikidata.org/sparql> {
+            OPTIONAL { ?item wdt:P18 ?image_url. }
+            ?item rdfs:label ?personLabel
+            SERVICE wikibase:label { 
+                bd:serviceParam wikibase:language "pt". 
+                ?item rdfs:label ?personLabel 
+            }
+      }
+    }        """
+    result = query_sparql(prefixes + "\n" + query, "local")
+    persons = []
+    for x in result["results"]["bindings"]:
+        image_url = None
+        if 'image_url' in x:
+            image_url = x["image_url"]["value"]
+        persons.append(
+            {'wiki_id': x["item"]["value"],
+             'name': x["personLabel"]["value"],
+             'image_url': image_url,
+            }
+        )
+    return persons
+
+
+def main():
+    data = get_persons()
+    with open('persons.json', 'wt') as f_out:
+        json.dump(data, f_out, indent=True, sort_keys='True')
+
+
+if __name__ == '__main__':
+    main()
