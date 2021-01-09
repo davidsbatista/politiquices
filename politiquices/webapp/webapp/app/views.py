cf0e9cb475ce46c2ec6ce3344b44e5584a3ef099
David Soares Batista <david.batista@comtravo.com>
2021-01-09 02:10:00 +0100

    tabbed tables

--- politiquices/webapp/webapp/app/views.py
+++ politiquices/webapp/webapp/app/views.py
@@ -19,7 +19,7 @@ from politiquices.webapp.webapp.app.sparql_queries import (
     get_wiki_id_affiliated_with_party,
     list_of_spec_relations_between_a_person_and_members_of_a_party,
     list_of_spec_relations_between_members_of_a_party_with_someone,
-    list_of_spec_relations_between_two_parties
+    list_of_spec_relations_between_two_parties,
 )
 
 from politiquices.webapp.webapp.app.relationships import build_relationships_freq
@@ -43,21 +43,25 @@ with open("webapp/app/static/json/party_members.json") as f_in:
 entities_batch_size = 16
 
 
+# Landing Page
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+# Procurar
 @app.route("/search")
 def search():
     return render_template("search.html")
 
 
+# Sobre
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
+# Estatísticas
 @app.route("/stats")
 def status():
     year, nr_articles_year = get_nr_articles_per_year()
@@ -72,6 +76,13 @@ def status():
     return render_template("stats.html", items=items)
 
 
+# Personalidades (first call)
+@app.route("/entities")
+def list_entities():
+    return render_template("all_entities.html", items=all_entities_info[0:entities_batch_size])
+
+
+# Personalidades (AJAX calls after first call)
 @app.route("/load_entities")
 def load_entities():
     start = int(request.args.get("last_index"))
@@ -80,33 +91,28 @@ def load_entities():
     return jsonify(all_entities_info[start:end])
 
 
-@app.route("/entities")
-def list_entities():
-    return render_template("all_entities.html", items=all_entities_info[0:entities_batch_size])
-
-
 def make_title_linkable(r, wiki_id):
 
     # add link to focus entity
-    link_one = r['title'].replace(
-        r['focus_ent'],
-        '<a id="ent_1" href="entity?q=' + wiki_id + '">' + r['focus_ent'] + '</a>'
+    link_one = r["title"].replace(
+        r["focus_ent"], '<a id="ent_1" href="entity?q=' + wiki_id + '">' + r["focus_ent"] + "</a>"
     )
     # add link to other entity page
     title_link = link_one.replace(
-        r['other_ent_name'],
-        '<a id="ent_2" href=' + r['other_ent_url'] + '>' + r['other_ent_name'] + '</a>'
+        r["other_ent_name"],
+        '<a id="ent_2" href=' + r["other_ent_url"] + ">" + r["other_ent_name"] + "</a>",
     )
-    r['title_clickable'] = title_link
+    r["title_clickable"] = title_link
 
-    if r['url'].startswith('http://publico.pt'):
-        r['link_image'] = "/static/images/114px-Logo_publico.png"
-        r['image_width'] = "20"
+    if r["url"].startswith("http://publico.pt"):
+        r["link_image"] = "/static/images/114px-Logo_publico.png"
+        r["image_width"] = "20"
     else:
-        r['link_image'] = "/static/images/color_vertical.svg"
-        r['image_width'] = "39.8"
+        r["link_image"] = "/static/images/color_vertical.svg"
+        r["image_width"] = "39.8"
 
 
+# Personalidade View: called from 'Personalidade'-nav-bar or 'Personalidades'-click
 @app.route("/entity")
 def detail_entity():
     from_search = False
@@ -115,7 +121,6 @@ def detail_entity():
         from_search = True
 
     person = get_person_info(wiki_id)
-
     top_entities_in_rel_type = get_top_relationships(wiki_id)
     relationships_articles = get_person_relationships(wiki_id)
 
@@ -168,10 +173,68 @@ def detail_entity():
         "supported_by_freq": supported_by_freq,
     }
 
-    if from_search:
-        return render_template("entity_timeline.html", items=items)
+    all_relationships_json = []
 
-    return render_template("entity.html", items=items)
+    opposed_json = []
+    for i in items["opposed"]:
+        # title with entities clickable and icon with link to arquivo.pt
+        html_title = f"""
+            {i['title_clickable']} <a id="link" href={i['url']} target="_blank">
+            <img src="{i['link_image']}" width="{i['image_width']}" height="20"></a>
+            """
+        opposed_json.append({"data": i["date"], "titulo": html_title})
+        all_relationships_json.append({"data": i["date"], "titulo": html_title})
+
+    supported_json = []
+    for i in items["supported"]:
+        # title with entities clickable and icon with link to arquivo.pt
+        html_title = f"""
+            {i['title_clickable']} <a id="link" href={i['url']} target="_blank">
+            <img src="{i['link_image']}" width="{i['image_width']}" height="20"></a>
+            """
+        supported_json.append({"data": i["date"], "titulo": html_title})
+        all_relationships_json.append({"data": i["date"], "titulo": html_title})
+
+    opposed_by_json = []
+    for i in items["opposed_by"]:
+        # title with entities clickable and icon with link to arquivo.pt
+        html_title = f"""
+            {i['title_clickable']} <a id="link" href={i['url']} target="_blank">
+            <img src="{i['link_image']}" width="{i['image_width']}" height="20"></a>
+            """
+        opposed_by_json.append({"data": i["date"], "titulo": html_title})
+        all_relationships_json.append({"data": i["date"], "titulo": html_title})
+
+    supported_by_json = []
+    for i in items["supported_by"]:
+        # title with entities clickable and icon with link to arquivo.pt
+        html_title = f"""
+            {i['title_clickable']} <a id="link" href={i['url']} target="_blank">
+            <img src="{i['link_image']}" width="{i['image_width']}" height="20"></a>
+            """
+        supported_by_json.append({"data": i["date"], "titulo": html_title})
+        all_relationships_json.append({"data": i["date"], "titulo": html_title})
+
+    if from_search:
+        return render_template(
+            "entity.html",
+            items=items,
+            opposed=opposed_json,
+            supported=supported_json,
+            opposed_by=opposed_by_json,
+            supported_by=supported_by_json,
+            all_relationships=all_relationships_json,
+        )
+
+    return render_template(
+        "entity.html",
+        items=items,
+        opposed=opposed_json,
+        supported=supported_json,
+        opposed_by=opposed_by_json,
+        supported_by=supported_by_json,
+        all_relationships=all_relationships_json,
+    )
 
 
 @app.route("/party_members")
@@ -180,25 +243,27 @@ def party_members():
 
     # get party info
     for x in all_parties_info:
-        if x['wiki_id'] == wiki_id:
-            party_name = x['party_label']
-            party_logo = x['party_logo']
+        if x["wiki_id"] == wiki_id:
+            party_name = x["party_label"]
+            party_logo = x["party_logo"]
 
     # get all members
     persons = []
     members_id = all_parties_members[wiki_id]
     for member_id in members_id:
         for entity in all_entities_info:
-            if member_id == entity['wikidata_id']:
+            if member_id == entity["wikidata_id"]:
                 persons.append(
                     Person(
-                        name=entity['name'],
-                        wiki_id=entity['wikidata_id'],
-                        image_url=entity['image_url'],
+                        name=entity["name"],
+                        wiki_id=entity["wikidata_id"],
+                        image_url=entity["image_url"],
                     )
                 )
 
-    return render_template("party_members.html", items=persons, name=party_name, logo=party_logo)
+    return render_template(
+        "party_members.html", items=persons, name=party_name, logo=party_logo, party_wiki_id=wiki_id
+    )
 
 
 @app.route("/parties")
@@ -226,27 +291,27 @@ def complete():
 
 def make_title_linkable_2_entities(r):
 
-    ent1_wikid_id = r['ent1'].split("/")[-1]
-    link_one = r['title'].replace(
-        r['ent1_str'],
-        '<a id="ent_1" href="entity?q=' + ent1_wikid_id + '">' + r['ent1_str'] + '</a>'
+    ent1_wikid_id = r["ent1"].split("/")[-1]
+    link_one = r["title"].replace(
+        r["ent1_str"],
+        '<a id="ent_1" href="entity?q=' + ent1_wikid_id + '">' + r["ent1_str"] + "</a>",
     )
 
-    ent2_wikid_id = r['ent2'].split("/")[-1]
+    ent2_wikid_id = r["ent2"].split("/")[-1]
     title_link = link_one.replace(
-        r['ent2_str'],
-        '<a id="ent_2" href="entity?q=' + ent2_wikid_id + '">' + r['ent2_str'] + '</a>'
+        r["ent2_str"],
+        '<a id="ent_2" href="entity?q=' + ent2_wikid_id + '">' + r["ent2_str"] + "</a>",
     )
 
-    r['title_clickable'] = title_link
+    r["title_clickable"] = title_link
 
-    if r['url'].startswith('http://publico.pt'):
-        r['link_image'] = "/static/images/114px-Logo_publico.png"
-        r['image_width'] = "20"
+    if r["url"].startswith("http://publico.pt"):
+        r["link_image"] = "/static/images/114px-Logo_publico.png"
+        r["image_width"] = "20"
 
     else:
-        r['link_image'] = "/static/images/color_vertical.svg"
-        r['image_width'] = "39.8"
+        r["link_image"] = "/static/images/color_vertical.svg"
+        r["image_width"] = "39.8"
 
 
 @app.route("/queries")
@@ -256,7 +321,7 @@ def queries():
     query_nr = request.args.get("query_nr")
 
     # relationships between two persons
-    if query_nr == 'two':
+    if query_nr == "two":
         person_one = request.args.get("e1")
         person_two = request.args.get("e2")
         results = get_relationships_between_two_entities(person_one, person_two)
@@ -307,8 +372,8 @@ def queries():
     if query_nr == "five":
         party_a = request.args.get("party_a")
         party_b = request.args.get("party_b")
-        party_a = ' '.join(['wd:'+x for x in get_wiki_id_affiliated_with_party(party_a)])
-        party_b = ' '.join(['wd:'+x for x in get_wiki_id_affiliated_with_party(party_b)])
+        party_a = " ".join(["wd:" + x for x in get_wiki_id_affiliated_with_party(party_a)])
+        party_b = " ".join(["wd:" + x for x in get_wiki_id_affiliated_with_party(party_b)])
 
         relationship = request.args.get("relationship")
         if relationship == "opoe-se":
