cb27fd33a9f5889d7e2721b6303c293b420d88bf
David S. Batista <david.batista@comtravo.com>
2021-02-19 15:19:49 +0100

    revert port 5000

--- politiquices/webapp/webapp/run_webserver.py
+++ politiquices/webapp/webapp/run_webserver.py
@@ -236,7 +236,7 @@ def main():
     persons_relationships_counts_by_type()
     """
 
-    app.run(debug=True, host="0.0.0.0", port=int("80"))
+    app.run(debug=True, host="0.0.0.0")
 
 
 if __name__ == "__main__":
