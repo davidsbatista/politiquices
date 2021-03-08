4d167485b15c9f03b4f1a58efdaf5682ce30f794
David S. Batista <david.batista@comtravo.com>
2021-03-09 00:00:54 +0100

    refactored direction clf

--- politiquices/classifiers/rel_direction/evaluate.py
+++ politiquices/classifiers/rel_direction/evaluate.py
@@ -1,7 +1,6 @@
-import pt_core_news_lg
 from sklearn.metrics import classification_report
 
-from politiquices.classifiers.rel_direction.relationship_direction_clf import detect_direction
+from politiquices.classifiers.rel_direction.relationship_direction_clf import DirectionClassifier
 from politiquices.extraction.utils.utils import (
     clean_title_quotes,
     clean_title_re,
@@ -10,21 +9,15 @@ from politiquices.extraction.utils.utils import (
 
 
 def main():
-    nlp = pt_core_news_lg.load()
-    nlp.disable = ["tagger", "parser", "ner"]
-
-    data_publico = read_ground_truth("../../../../data/annotated/publico_politica.tsv", only_label=True)
-    data_arquivo = read_ground_truth("../../../../data/annotated/arquivo.tsv", only_label=True)
-    data_webapp = read_ground_truth("../../api_annotations/annotations_from_webapp.csv", delimiter=",", only_label=True)
-
+    publico = read_ground_truth("../../../data/annotated/publico.csv")
+    arquivo = read_ground_truth("../../../data/annotated/arquivo.csv")
+    direction_clf = DirectionClassifier()
     true_direction = []
     pred_direction = []
 
-    for d in data_publico + data_arquivo + data_webapp:
+    for d in publico + arquivo:
         if "supports" in d["label"] or "opposes" in d["label"]:
-
             clean_title = clean_title_quotes(clean_title_re(d['title']))
-            doc = nlp(clean_title)
             ent1 = d["ent1"]
             ent2 = d["ent2"]
 
@@ -32,25 +25,20 @@ def main():
                 print("skipped")
                 continue
 
-            if d["label"].endswith("ent1"):
-                true_direction.append("ent2")
-                true = "ent2"
-            else:
-                true_direction.append("ent1")
-                true = "ent1"
-
-            pos_tags = [(t.text, t.pos_, t.tag_) for t in doc]
-            pred, pattern = detect_direction(pos_tags, ent1, ent2)
+            true = "ent2_rel_ent1" if d["label"].endswith("ent1") else "ent1_rel_ent2"
+            true_direction.append(true)
+            pred, pattern = direction_clf.detect_direction(clean_title, ent1, ent2)
             pred_direction.append(pred)
 
+            """
             if true != pred:
                 print("true: ", true)
                 print("pred: ", pred)
                 print(d["title"], "\t", d["label"])
-                print(pos_tags)
                 print(pattern)
                 print()
                 print("\n-----------------------------")
+            """
 
     print(classification_report(true_direction, pred_direction))
 
