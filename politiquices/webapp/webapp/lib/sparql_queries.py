d79d8e03e82aa9c8cf1b6b13d6194f0403a5a154
David S. Batista <david.batista@comtravo.com>
2021-03-17 23:47:41 +0100

    nit

--- politiquices/webapp/webapp/lib/sparql_queries.py
+++ politiquices/webapp/webapp/lib/sparql_queries.py
@@ -1140,7 +1140,7 @@ def get_relationships_to_annotate():
           FILTER(REGEX(?rel_type,"other")).
         }
         ORDER BY ?date ?score
-        LIMIT 5000
+        LIMIT 2500
     """
     result = query_sparql(PREFIXES + "\n" + query, "politiquices")
     to_annotate = []
