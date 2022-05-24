b4af361419ddd087db9881da0762199d662e24c5
David S. Batista <dsbatista@gmail.com>
2022-05-24 21:57:16 +0200

    just pointing to new repo

--- politiquices/webapp/fuseki-docker/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/jena/bin/tdbloader2 -d -l /fuseki/wikidata /usr/share/data/wikidata_org.ttl.bz2
-/jena/bin/tdbloader2 -d -l /fuseki/politiquices /usr/share/data/politiquices.ttl
-/jena-fuseki/fuseki-server -v --config=/fuseki/config.ttl
--- politiquices/webapp/webapp/app/wsgi.py
+++
@@ -1,4 +0,0 @@
-from politiquices.webapp.webapp.app.politiquices_app import app
-
-if __name__ == "__main__":
-    app.run()
